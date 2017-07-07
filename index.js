2bfcd8b5ead4800608c12d5e4e68e1e54ba492da
teuteuguy <pub@lalatte.com>
2017-07-07 16:26:17 +0800

    Update

--- index.js
+++ index.js
@@ -29,20 +29,17 @@ var thingState = {
 };
 
 console.log('[SETUP] thingShadow state initialized with:');
-console.log(thingState);
+console.log(JSON.stringify(thingState, null, 2));
 console.log('[SETUP] Initializing IoT thingShadow with config:');
-console.log(configIoT);
+console.log(JSON.stringify(configIoT, null, 2));
 
 var thingShadow = awsIot.thingShadow(configIoT);
 
-var clientTokenUpdate;
-var refreshRequired = false;
-
-setInterval(function() {
-    console.log('Here');
-}, 0);
+var clientTokenUpdate = null;
 
 function refreshShadow() {
+
+    console.log('[EVENT] refreshShadow(): clientTokenUpdate:', clientTokenUpdate);
 
     var toUpdate = {
         state: {
@@ -50,15 +47,12 @@ function refreshShadow() {
         }
     };
 
-    console.log('[EVENT] refreshShadow(): Update shadow:');
-    console.log(JSON.stringify(toUpdate, null, 2));
+    console.log('[EVENT] refreshShadow(): Update shadow:', JSON.stringify(toUpdate));
 
     clientTokenUpdate = thingShadow.update(config.iotThingName, toUpdate);
 
     if (clientTokenUpdate === null) {
         console.log('[EVENT] refreshShadow(): update shadow failed, operation still in progress');
-    } else {
-        sense.clear(thingState.color.r, thingState.color.g, thingState.color.b);
     }
 }
 
@@ -96,6 +90,7 @@ thingShadow.on('status', function(thingName, stat, clientToken, stateObject) {
     console.log('[IOT EVENT] thingShadow.on(status): clientToken:      ', clientToken);
     console.log('[IOT EVENT] thingShadow.on(status): clientTokenUpdate:', clientTokenUpdate);
     console.log('[IOT EVENT] thingShadow.on(status): stateObject:', stateObject);
+    if (clientToken === clientTokenUpdate) clientTokenUpdate = null;
 });
 
 thingShadow.on('message', function(topic, payload) {
@@ -114,9 +109,9 @@ thingShadow.on('delta', function(thingName, stateObject) {
         if (stateObject.state.color.b !== undefined) thingState.color.b = stateObject.state.color.b;
     }
 
-    console.log('[EVENT] thingShadow.on(delta): Updated thingState to:');
-    console.log(JSON.stringify(thingState, null, 2));
+    console.log('[EVENT] thingShadow.on(delta): Updated thingState to:', JSON.stringify(thingState));
 
-    // setTimeout(refreshShadow, 0);
+    sense.clear(thingState.color.r, thingState.color.g, thingState.color.b);
+
     refreshShadow();
 });
